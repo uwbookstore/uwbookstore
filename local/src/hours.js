602c2bfd7a9e6b0b804a8663b21138342012d4c7
ClaytonDewey <cdewey@uwbookstore.com>
2023-08-28 14:47:06 -0500

    Update hours for the week.

--- local/src/hours.js
+++ local/src/hours.js
@@ -44,8 +44,8 @@ function getStateStHrs() {
         <li><span class="hours__day">Tuesday:</span> <span class="hours__times">9:00am - 7:00pm</span></li>
         <li><span class="hours__day">Wednesday:</span> <span class="hours__times">9:00am - 7:00pm</span></li>
         <li><span class="hours__day">Thursday:</span> <span class="hours__times">9:00am - 7:00pm</span></li>
-        <li><span class="hours__day">Friday:</span> <span class="hours__times">9:00am - 5:30pm</span></li>
-        <li><span class="hours__day">Saturday:</span> <span class="hours__times">9:00am - 5:30pm</span></li>
+        <li><span class="hours__day">Friday:</span> <span class="hours__times">9:00am - 7:00pm</span></li>
+        <li><span class="hours__day">Saturday:</span> <span class="hours__times">8:30am - 5:30pm</span></li>
     </ul>
 `;
 
@@ -59,18 +59,18 @@ function getStateStHrs() {
 
     switch (day) {
       case 1:
-        case 2:
-        case 3:
+      case 2:
+      case 3:
       case 4:
+      case 5:
         open = 9;
         openMin = 0;
         close = 19;
         closeMin = 0;
         break;
-      case 5:
       case 6:
-        open = 9;
-        openMin = 0;
+        open = 3;
+        openMin = 30;
         close = 17;
         closeMin = 30;
         break;

aa54e433f71839a76c75e57695051834ac2e57d8
ClaytonDewey <clay@dryadmedia.com>
2023-10-02 12:39:06 -0500

    Save all page specific head/footer includes

---
+++ bs5/inSite/Header-Footer/InSite Pages/AddressBook/Footer-Script.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+/* $(document).ready(function () {
+  $(".addAddressStudentNumber").attr("placeholder", "Student ID/Phone Number *");
+  if (!$(".addAddressStudentNumber").val().trim().length) {
+    $(".addAddressStudentNumber").removeAttr("readonly");
+  }
+}); */
+</script>

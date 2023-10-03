124537c8e8d47847d6c3e2a4316df698acbf4f49
ClaytonDewey <clay@dryadmedia.com>
2023-10-03 04:59:38 -0500

    Update Wiscard promo for new checkout page

--- local/build/wiscard.js
+++ local/build/wiscard.js
@@ -1,94 +1,129 @@
+// Auto apply promocode when Wiscard option is selected
+// Optional: auto remove promo code if Wiscard is deselected and applied promo code is Wiscard promo code
+const uwWiscardPromo = 'WISCARD4';
+const uwWiscardPaymentType = '1651';
+
+// We need to hook into ajaxComplete function since payment section is not present on page load
+$(document).on('ajaxComplete', function (event, xhr, settings) {
+  // check if json returned by ajax is showPayment = true.
+  // if so, then the payment section was just rendered and inserted into the dom.
+  // time to add our events
+
+  if (xhr.responseJSON.showPayment) {
+    // bind change event
+    $('input[name=paymentType]').change(function () {
+      let ptId = $(this).val();
+      if (ptId == uwWiscardPaymentType) {
+        // check that account number was input automatically
+        if (!$('#accountNumber-input').length > 0) return;
+
+        // passed check, add and apply promo
+        $('#coPromoCode').val(uwWiscardPromo);
+        $('#promo-apply').click();
+      } else {
+        // optional remove this else statement to prevent promo from being removed if payment type is changed away from set payment type
+        if ($('#coPromoCode').val() == uwWiscardPromo) $('#promo-apply').click();
+      }
+    });
+  }
+});
+
 $(document).ready(function () {
-  // $("#applyPromoBtn").hide();
-  // Check for WisCard as payment option
-  // and verify there isn't a promo code already
-  $('#campusAccount').hide();
+  // Shipping Name
+  $('input[name=coSelectShipping]').each(function () {
+    // get element object
+    let nameElement = $(this).next('.newRadioText');
 
-  if ($('.accountNumberTitle').length > 0) {
-    if ($('input#promoCode').val().trim() === '') {
-      $('input#promoCode').val('WISCARD4');
-      $('#applyPromoBtnHidden').click();
+    // get current name of ship method
+    let name = $(nameElement).text();
+
+    // default to true, this will flip to false if no case was found through switch
+    let nameCaseFound = true;
+
+    // initialize empty string
+    let newName = '';
+
+    switch (name) {
+      case 'GC Only Ground est. $3.99':
+        newName = 'Gift Card Only Ground est. $3.50';
+        break;
+      case 'GC 2nd Day est. $20.00':
+        newName = 'Gift Card 2nd Day est $20.00';
+        break;
+      case 'GC Next Day est. $30.00':
+        newName = 'Gift Card Next Day est. $30.00';
+        break;
+      default:
+        nameCaseFound = false;
+        break;
     }
-  }
 
-  if ($('input#promoCode').val() === 'WISCARD4') {
-    $('input#promoCode').css('color', '#fff').attr('onfocus', "this.value=''");
-    $('p#lblValidPromoMsg').hide();
-  }
+    // if nameCaseFound did not switch to true, then we found a valid case and should set text
+    if (nameCaseFound) $(nameElement).text(newName);
+  });
 
-  // Suppress promo code error
-  // for WISCARD promo when
-  // inelligible item is selected
-  if ($('input#promoCode').val() === 'WISCARD4' && $("div.validation-summary-errors ul li:contains('You do not have items in your shopping cart that qualify for the promotion.')").length) {
-    $("div.validation-summary-errors ul li:contains('You do not have items in your shopping cart that qualify for the promotion.')").hide();
-  }
+  // Shipping description
+  $('input[name=coSelectShipping]').change(function () {
+    // get shipId value
+    let shipId = $(this).val();
 
-  // If promo code is entered,
-  // force "click" of #applyPromoBtn
-  // $("#promoCode").blur(function () {
-  //   if ($(this).val().trim() !== "") {
-  //     $("#applyPromoBtn").click();
-  //   }
-  // });
+    // default to true, this will flip to false if no case was found through switch
+    let shipCaseFound = true;
 
-  $('#shipdesc').removeClass('col-xs-12 bottom10');
-  $('#campusVerify').addClass('alert alert-danger');
-  $('<p class="center red">Gift Card Verification Checkbox</p>').prependTo('#campusVerify');
+    // every time we change the shipping method, just remove msg text div from page
+    $('#msg-text').remove();
 
-  $('<div id="msg-text"></div>').appendTo('#shipdesc').hide();
+    // initialize message to empty string
+    let message = '';
 
-  $('select#selectedMethod').change(function () {
-    const shipType = $(this).val();
-
-    switch (shipType) {
+    switch (shipId) {
       case '990':
       case '986':
       case '987':
-        $('#shipdesc').show();
-        $('#msg-text').html('<div class="alert alert-danger">This shipping method is valid for the shipping of Gift Cards only. If you choose this method and are purchasing anything other than a gift card, the shipping method will be corrected to "Ground Shipping" and you will be charged accordingly.<br /><br />Your order will be shipped via UPS&reg;, FedEx&reg; or USPS&reg; which will take 1-3 business days (Mon-Fri, dependent on location and shipping method chosen) to reach you.</div>').show();
+        message = '<div class="alert alert-danger">This shipping method is valid for the shipping of Gift Cards only. If you choose this method and are purchasing anything other than a gift card, the shipping method will be corrected to "Ground Shipping" and you will be charged accordingly.<br /><br />Your order will be shipped via UPS&reg;, FedEx&reg; or USPS&reg; which will take 1-3 business days (Mon-Fri, dependent on location and shipping method chosen) to reach you.</div>';
         break;
       case '646':
       case '649':
-        $('#shipdesc').show();
-        $('#msg-text').html('<div class="alert alert-danger">Our order processing time is 1-2 business days (Mon-Fri) for expedited orders.  After your order is processed it will be shipped via UPS&reg;, FedEx&reg; or USPS&reg; which will take 1-2 business days (Mon-Fri, dependent on location and shipping method chosen) to reach you.</div>').show();
+        message = '<div class="alert alert-danger">Our order processing time is 1-2 business days (Mon-Fri) for expedited orders.  After your order is processed it will be shipped via UPS&reg;, FedEx&reg; or USPS&reg; which will take 1-2 business days (Mon-Fri, dependent on location and shipping method chosen) to reach you.</div>';
         break;
       case '1278':
-        $('#shipdesc').show();
-        $('#msg-text').html('<div class="alert alert-danger">This shipping method is to be used for International Orders only. If you choose this method and are shipping domestically, the shipping method will be corrected to "Ground Shipping" and you will be charged accordingly.</div>').show();
+        message = '<div class="alert alert-danger">This shipping method is to be used for International Orders only. If you choose this method and are shipping domestically, the shipping method will be corrected to "Ground Shipping" and you will be charged accordingly.</div>';
         break;
       case '2193':
-        $('#shipdesc').show();
-        $('#msg-text').html('<div class="alert alert-danger">This shipping method is to be used for orders over $75 (pre-tax subtotal). If you choose this method and your order total is less than $75, the shipping method will be corrected to "Ground Shipping" and you will be charged accordingly.</div>').show();
+        message = '<div class="alert alert-danger">This shipping method is to be used for orders over $75 (pre-tax subtotal). If you choose this method and your order total is less than $75, the shipping method will be corrected to "Ground Shipping" and you will be charged accordingly.</div>';
         break;
       case '2298':
-        $('#shipdesc').show();
-        $('#msg-text').html('<div class="alert alert-danger">*$1.28 flat rate ground shipping to a contiguous U.S. delivery address. Excludes Lands\' End items, textbooks, all StandardChair of Gardner products, custom orders, tech items, and drop-ship items. <strong>Orders with any excluded items will be charged the full standard shipping fee.</strong> All items with a handling charge will receive $1.28 ground shipping but handling charge will still be applied.</div>').show();
+        '<div class="alert alert-danger">*$1.28 flat rate ground shipping to a contiguous U.S. delivery address. Excludes Lands\' End items, textbooks, all StandardChair of Gardner products, custom orders, tech items, and drop-ship items. <strong>Orders with any excluded items will be charged the full standard shipping fee.</strong> All items with a handling charge will receive $1.28 ground shipping but handling charge will still be applied.</div>';
+        break;
+      case '970':
+      case '1626':
+        message = '<div id="shipDisclaimer" class="alert alert-danger">Please note: Pickup at Brookfield location can take up to 7 days.  Please click on the Shipping Policy above for details.</div>';
+        break;
+      case '1957':
+        message = '<div id="shipDisclaimer" class="alert alert-danger"><strong>CONTACTLESS PICK-UP AVAILABLE</strong><br>Park in front of sandwich board in front of store.<br>Call 310-5976 when you arrive.</div>';
+        break;
+      case '968':
+        message = '<div id="shipDisclaimer" class="alert alert-danger"><strong>CONTACTLESS PICK-UP AVAILABLE</strong><br>Park in valid parking spots directly across from the store<br>Call 310-5980 when you arrive.</div>';
         break;
       default:
-        $('#msg-text').hide();
+        shipCaseFound = false;
+        break;
+    }
+
+    // if shipCaseFound did not flip to false, we found a valid case
+    if (shipCaseFound) {
+      // create our msg text div and append it to the radio input's parent
+      $('<div id="msg-text"></div>').appendTo($(this).parent());
+
+      // set the message
+      $('#msg-text').html(message);
+
+      // hide default built in inSite ship description
+      $(this).parent().find('.shippingMethod-description-span').css('display', 'none');
     }
   });
 
-  $("select#selectedMethod option:contains('GC Only Ground est. $3.50')").text('Gift Card Only Ground est. $3.50');
-  $("select#selectedMethod option:contains('GC 2nd Day est. $20.00')").text('Gift Card 2nd Day est. $20.00');
-  $("select#selectedMethod option:contains('GC Next Day est. $30.00')").text('Gift Card Next Day est. $30.00');
-
-  const pickUpLocation = $('select#selectedMethod').val();
-
-  switch (pickUpLocation) {
-    case '970':
-    case '1626':
-      $('<div id="shipDisclaimer" class="alert alert-danger">Please note: Pickup at Brookfield location can take up to 7 days.  Please click on the Shipping Policy above for details.</div>').insertAfter('select#selectedMethod');
-      break;
-    case '1957':
-      $('<div id="shipDisclaimer" class="alert alert-danger"><strong>CONTACTLESS PICK-UP AVAILABLE</strong><br>Park in front of sandwich board in front of store.<br>Call 310-5976 when you arrive.</div>').insertAfter('select#selectedMethod');
-      break;
-    case '968':
-      $('<div id="shipDisclaimer" class="alert alert-danger"><strong>CONTACTLESS PICK-UP AVAILABLE</strong><br>Park in valid parking spots directly across from the store<br>Call 310-5980 when you arrive.</div>').insertAfter('select#selectedMethod');
-      break;
-    default:
-      break;
-  }
-
-  $(`<p class="bold red mb-0">International Shipping unavailable at this time.</p>`).insertAfter('#shipPolicyLink');
+  // International Shipping Notice
+  $(`<p class="bold red mb-0">International Shipping unavailable at this time.</p>`).insertAfter('#shippingPolicy');
 });
+Save;

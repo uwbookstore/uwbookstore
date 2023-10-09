293795e9c4253c4e93a595481d10dee77416228a
ClaytonDewey <clay@dryadmedia.com>
2023-10-09 11:03:46 -0500

    Rewrite MBS shopping cart if empty

--- src/checkout.js
+++ src/checkout.js
@@ -1,12 +1,10 @@
 $(document).ready(() => {
   const baseUrl = 'https://www.uwbookstore.com/';
   const itemCount = $('span#cart-itemCount').text();
-  console.log('Loaded...');
+
   if (itemCount === '0') {
-    console.log('Empty cart');
     if (baseUrl === 'https://www.uwbookstore.com/') {
-      console.log(`We're on the right site...`);
-      $('.cart-leftCard-header .card-header').html(`
+      $(`
       <div class="cart__container"><i class="cart__icon fa fa-shopping-cart" aria-hidden="true"></i>
         <p class="cart__p">Your shopping cart is empty.</p><a class="btn btn-primary cart__btn" href="${baseUrl}" title="Shop Clothing &amp; Gifts">Shop Clothing &amp; Gifts</a>
         <div class="cart__account">
@@ -15,7 +13,8 @@ $(document).ready(() => {
         </div>
       </div>
       </div>      
-      `);
+      `).insertAfter('.cart-leftCard-header');
+      $('.cart-leftCard-header').hide();
     }
   }
 });

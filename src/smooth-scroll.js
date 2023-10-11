8adaf8c11c2c21f4afb1ed67ab409338747d9b98
ClaytonDewey <clay@dryadmedia.com>
2023-10-11 10:31:51 -0500

    feat: Add smooth scroll animation

---
+++ src/smooth-scroll.js
@@ -0,0 +1,15 @@
+$(function() {
+  $("a.page-scroll").bind("click", function() {
+    var $anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top
+        },
+        1500,
+        "easeInOutExpo"
+      );
+    event.preventDefault();
+  });
+});

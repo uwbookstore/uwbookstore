b51f2061e79074db04268bc1963ffa4a7c38659d
ClaytonDewey <clay@dryadmedia.com>
2024-09-05 09:18:33 -0500

    chore: Update spring return deadline

--- src/text-return.js
+++ src/text-return.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   //Textbook Return policy
   $('.retPolicy').html(
-    'You may return your textbooks with a receipt for a full refund through Wednesday, September 11<sup>th</sup> (2024 Fall Semester), and through Tuesday, January 30<sup>th</sup> (2025 Spring Semester).'
+    'You may return your textbooks with a receipt for a full refund through Wednesday, September 11<sup>th</sup> (2024 Fall Semester), and through Tuesday, January 28<sup>th</sup> (2025 Spring Semester).'
   );
 });

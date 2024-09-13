1508259bb440f6f0b904ca8416067fbbb78d1bd7
ClaytonDewey <clay@dryadmedia.com>
2024-09-13 08:45:16 -0500

    feat: Add login options buttons to page

---
+++ src/login-options.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('.SSOLoginLink').addClass('m-2').appendTo('#SSOLoginLink-box');
+
+  $('.nonSSOLoginLink').addClass('m-2').appendTo('#nonSSOLoginLink-box');
+
+  $('#loginOptionsPanel').hide();
+});

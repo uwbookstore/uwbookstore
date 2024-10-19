4aa58f3930040325a38025926e3692a1e107c700
ClaytonDewey <clay@dryadmedia.com>
2024-10-19 12:12:03 -0500

    chore: Update countdown timer date

--- src/countdown.js
+++ src/countdown.js
@@ -26,7 +26,7 @@ const countdownWrapper = document.querySelector('.countdown__wrapper');
 const items = document.querySelectorAll('.timer__box p');
 
 // months are ZERO index based;
-const futureDate = new Date(Date.UTC(2024, 9, 19, 16, 0, 0));
+const futureDate = new Date(Date.UTC(2024, 9, 26, 23, 30, 0));
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();

b2db0eda7877d49bd85ec96e93cee353b6088f4c
ClaytonDewey <clay@dryadmedia.com>
2024-11-26 10:45:28 -0600

    chore: Update Wiscard promo code

--- src/wiscard.js
+++ src/wiscard.js
@@ -1,6 +1,6 @@
 // Auto apply promocode when Wiscard option is selected
 // Optional: auto remove promo code if Wiscard is deselected and applied promo code is Wiscard promo code
-const uwWiscardPromo = 'WISCARD3';
+const uwWiscardPromo = 'WISCARD4';
 
 // We need to hook into ajaxComplete function since payment section is not present on page load
 $(document).on('ajaxComplete', function (event, xhr, settings) {

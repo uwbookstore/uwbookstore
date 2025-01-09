24beafe98d7b29c83a6e2366fe360d1519faaf39
Clayton Dewey <clay@dryadmedia.com>
2025-01-09 12:34:27 -0600

    style: Rework merchlist page styles

--- src/merch-list.js
+++ src/merch-list.js
@@ -83,7 +83,7 @@ $(document).ready(function () {
   const pageItems = $('.pagination li');
 
   if (pageItems.length === 1) {
-    $('ul.pagination, select.merchResultsSelect').hide();
+    $('ul.pagination').hide();
   }
 
   if (window.location.href.search(/search-red/) !== -1) {
@@ -180,10 +180,47 @@ $(document).ready(function () {
 
   productName.each(function () {
     const name = $(this).text();
+    const prevSibling = $(this).prev();
     if (name.includes('^')) {
       $('<div class="merch__card-restock">Back in Stock!</div>').prependTo(
-        $(this).parent().parent().parent()
+        prevSibling
       );
+    }
+  });
+
+  const parentDiv = $('.panel-body');
+  $.each(parentDiv, function () {
+    if (!$(this).children().length > 0) {
+      $(this).parent().hide();
+    }
+  });
+
+  $(".category-name:contains('SPECIAL')").parent().parent().hide();
+  $(".category-name:contains('PACKAGE')").parent().parent().hide();
+  $(".category-name:contains('Study Aids')").parent().parent().hide();
+  $('.category-name:contains("WOMEN\'S HATS")').parent().parent().hide();
+  $('.category-name:contains("WINTER HATS")').parent().parent().hide();
+
+  const isUpperCase = (str) => str === str.toUpperCase();
+
+  const merchLink = $('.merchLinkText');
+  $.each(merchLink, function () {
+    $(this).removeClass('top5');
+    $(this).parent().removeClass('textc small displayb padding5');
+    if (isUpperCase($(this).text())) {
+      $(this).parent().parent().addClass('hide');
+    }
+
+    if ($(this).text() === 'cold&deg;gear') {
+      $(this).html('cold&deg;gear');
+    }
+
+    if ($(this).text() === 'heat&deg;gear') {
+      $(this).html('heat&deg;gear');
+    }
+
+    if ($(this).text() === 'Tokyodachi&reg;') {
+      $(this).html('Tokyodachi&reg;');
     }
   });
 });

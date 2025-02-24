4f6a4abcf81150d85a5d1fee3be79b96c5f36281
ClaytonDewey <clay@dryadmedia.com>
2025-02-24 16:57:56 -0600

    refactor: Call scrolltotop & coupon text loading

--- src/app.js
+++ src/app.js
@@ -2,6 +2,25 @@
 const backToTopBtn = document.getElementById('backtotop');
 
 // DEFINE ALL FUNCTIONS
+
+// Smooth scroll to top
+function scrollToTop(e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+// Show/Hide Scroll to Top button
+window.onscroll = function () {
+  if (scrollY >= 200) {
+    backToTopBtn.style.opacity = 1;
+  } else {
+    backToTopBtn.style.opacity = 0;
+  }
+};
+
 /**
  * Select an element by id and update innerText or innerHTML
  * @param {string} el - the element to be updated (id not class)
@@ -18,19 +37,19 @@ function fillInnerContent(el, text, type) {
   return null;
 }
 
-// Smooth scroll to top
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-}
+// CALL FUNCTIONS
+// Call scrolltotop
+backToTopBtn.addEventListener('click', scrollToTop);
 
-// Show/Hide Scroll to Top button
-window.onscroll = function () {
-  if (scrollY >= 200) {
-    backToTopBtn.style.opacity = 1;
-  } else {
-    backToTopBtn.style.opacity = 0;
-  }
-};
+/**
+ * Update the coupon code & expiration date
+ * for the various pages that use it
+ * (don't ask me why they chose to have multiple
+ *  pages for the same thing...it's a marketing thing)
+ */
+// Update coupon code/expiration
+// Across all current coupons
+fillInnerContent('coupon-header', '10% OFF', 'text');
+fillInnerContent('coupon-subhead', 'Your Entire Purchase!', 'text');
+fillInnerContent('coupon-code', '434', 'text');
+fillInnerContent('coupon-expiration', '6.30.25', 'text');

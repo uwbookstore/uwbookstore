50e7d4d0f4c7e91f5428b33dce247ba89d22f0d5
ClaytonDewey <clay@dryadmedia.com>
2025-04-09 14:22:43 -0500

    refactor: Insert default tabs

--- src/merch-detail.js
+++ src/merch-detail.js
@@ -11,21 +11,155 @@ const merchMain = `
     </div>
   </div>
   <h2 class="heading__line-center" id="tabs-header"><span>Additional Information</span></h2>
-  <div class="tabs">
-    <ul class="tabs__nav">
-      <li class="tabs__item">
-        <a href="#returns" class="tabs__link">
+  <div class="tabs-container additional-info">
+    <ul aria-labelledby="tabs-title">
+      <!-- <li>
+        <a id="tab-1" href="#disco">
+          <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Discontinued Item Policy
+        </a>
+      </li> -->
+      <li>
+        <a id="tab-2" href="#returns">
           <i class="fa fa-exchange" aria-hidden="true"></i> Returns &amp; Exchanges
         </a>
       </li>
-      <li class="tabs__item">
-        <a href="#shipping" class="tabs__link">
+      <li>
+        <a id="tab-3" href="#shipping">
           <i class="fa fa-truck" aria-hidden="true"></i> Shipping &amp; Handling
         </a>
       </li>
+      <!-- <li>
+        <a id="tab-4" href="#ratings">
+          <i class="fa fa-star" aria-hidden="true"></i> Reviews(0)
+        </a>
+      </li> -->
     </ul>
-  </div>
 
+    <div class="tabs__panels">
+      <div id="returns" aria-labelledby="tab-2">
+        <h2 class="tabs__header">Returns &amp; Exchanges</h2><br>
+        <div class="row">
+          <div class="col-md-6">
+            <h2 class="tabs__header">Non-Tech Returns &amp; Exchanges</h2>
+            <p>If you are not completely satisfied with any product, we will gladly replace it or refund the purchase
+              price
+              of the item. A non-text item may be returned any time as long as it was purchased from a University Book
+              Store
+              location or website and is in saleable condition. Returns without a receipt will be refunded at the
+              lowest
+              price
+              ever sold and credited to a University Book Store gift card. See associate for details.<br>Please do the
+              following:</p>
+            <ol>
+              <li>Items must be in new condition w/original packaging &amp; accessories.</li>
+              <li>Circle the item(s) on the packing list and note whether you want an EXCHANGE or CREDIT.</li>
+              <li>If an exchange, please make note as to what Size and/or Color you want.</li>
+            </ol>
+          </div>
+          <div class="col-md-6">
+            <h2 class="tabs__header">Tech Return Policy</h2>
+            <p>Tech items may be returned within 15 days, with receipt and in new condition, complete with all
+              packaging
+              and
+              pieces. Unopened product may be refunded in full and opened product will be subject to a 15% restocking
+              fee,
+              with the following exceptions:</p>
+            <ol>
+              <li>Open Batteries, graphing calculators, in-ear headphones, ink/toner, printers, and storage devices
+                are
+                not
+                returnable.</li>
+              <li>Special Order and sale items are generally not returnable- ask for details before purchasing please.
+              </li>
+              <li>Defective items may require warranty processing or may be exchanged for the same item after verified
+                defective, per manufacturer and store policies.</li>
+            </ol>
+          </div>
+        </div>
+        <br>
+        <p>Purchases may be returned to any of our stores with the packing list or send returns to:</p>
+        <address class="returns">University Book Store<br>
+          ATTN: Online Sales Returns Department<br>
+          4509 West Beltline Hwy<br>
+          Madison, WI 53711</address>
+        <p>Credit card purchases will be credited to the charge card used for the initial purchase. If the purchase
+          was
+          made
+          with a personal check, we will issue a check.</p>
+      </div>
+
+      <div id="shipping" aria-labelledby="tab-3">
+        <h2 class="tabs__header">Shipping &amp; Handling</h2>
+        <div class="text-center">
+          <img src="https://i.univbkstr.com/v3/img/misc/usps.png" alt="USPS Logo">
+          <img src="https://i.univbkstr.com/v3/img/misc/ups.png" alt="UPS Logo">
+        </div>
+        <p><strong>Methods for shipping are:</strong></p>
+        <table class="table table-striped">
+          <tbody>
+            <tr>
+              <td>Ground Shipping (5-7 days)</td>
+              <td>$7.00 + $0.50 for each item</td>
+            </tr>
+            <tr>
+              <td>2nd Day Air</td>
+              <td>$21.00 + $1.00 for each item</td>
+            </tr>
+            <tr>
+              <td>Next Day Air</td>
+              <td>$45.00 + $2.00 for each item</td>
+            </tr>
+          </tbody>
+        </table>
+        <p>Order processing time is 5-7 business days (for ground shipping) or 1-2 business days (for expedited and
+          pick
+          up
+          at store orders).</p>
+        <p><strong>Methods for shipping Gift Cards ONLY are:</strong></p>
+        <table class="table table-striped">
+          <tbody>
+            <tr>
+              <td>Gift Card (Ground)</td>
+              <td>$3.50</td>
+            </tr>
+            <tr>
+              <td>Gift Card (2nd Day Air)</td>
+              <td>$20.00</td>
+            </tr>
+            <tr>
+              <td>Gift Card (Next Day Air)</td>
+              <td>$30.00</td>
+            </tr>
+          </tbody>
+        </table>
+        <p>From the Delivery Methods section select your shipping method and then click "Continue".</p>
+        <p>Handling charges are applied to products that have special shipping requirements. Items that require
+          handling
+          charges are noted as such in the description field of that item.</p>
+        <p>Out of country orders are usually sent USPS International Priority Mail and will arrive in 6-10 business
+          days.
+          These orders are not shipped or charged until we can weigh the order and check with USPS for options /
+          prices.
+          We
+          will then e-mail that information to the customer for approval.</p>
+        <p>Items are shipped Monday through Friday between the hours of 8 AM and 3 PM.</p>
+        <p>We offer the option to "Pick Up At Store" at the following locations:<br>
+          Library Mall (711 State Street)<br>
+          Health Sciences Learning Center (750 N. Highland Ave)<br>
+          Hilldale Mall (454 N. Midvale Blvd)<br>
+          Please allow for our normal processing time of 3 – 4 business days. You will receive notification once the
+          order
+          has been delivered to the store of your choice.</p>
+        <p>Brookfield (95 North Moorland Road, Suite E2 - Brookfield)<br>
+          Please allow for a processing time of 4-8 business days. You will receive notification once the order has
+          been
+          delivered to the store of choice.</p>
+        <p>We are happy to work with customers who have particular shipping needs. Please feel free to <a
+            href="https://www.uwbookstore.com/Contact">email</a> us, or call us toll free at 800-993-2665 ext. 5997.
+        </p>
+      </div>
+    </div>
+  </div>
 `;
 
 // Prepend new wrapper to ContainDiv element

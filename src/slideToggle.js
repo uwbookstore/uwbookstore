fd1961ec8e81b7d880c7ac714ac49be0876fcab0
ClaytonDewey <clay@dryadmedia.com>
2025-04-16 10:30:08 -0500

    feat: Add slideDown function
    Make use of string literals in function

--- src/slideToggle.js
+++ src/slideToggle.js
@@ -6,9 +6,9 @@ function slideToggle(element, duration = 400) {
 
 function slideUp(element, duration = 400) {
   element.style.transitionProperty = 'height, margin, padding';
-  element.style.transitionDuration = duration + 'ms';
+  element.style.transitionDuration = `${duration}ms`;
   element.style.boxSizing = 'border-box';
-  element.style.height = element.offsetHeight + 'px';
+  element.style.height = `${element.offsetHeight}px`;
   element.offsetHeight; // force repaint
   element.style.overflow = 'hidden';
   element.style.height = 0;
@@ -27,5 +27,37 @@ function slideUp(element, duration = 400) {
     element.style.removeProperty = 'overflow';
     element.style.removeProperty = 'transition-duration';
     element.style.removeProperty = 'transition-property';
+  }, duration);
+}
+
+function slideDown(element, duration = 400) {
+  element.style.removeProperty('display');
+  let display = window.getComputedStyle(element).display;
+  if (display === 'none') display = 'block';
+  element.style.display = display;
+
+  const height = element.scrollHeight;
+  element.style.overflow = 'hidden';
+  element.style.height = 0;
+  element.style.paddingTop = 0;
+  element.style.paddingBottom = 0;
+  element.style.marginTop = 0;
+  element.style.marginBottom = 0;
+  element.offsetHeight; // force repaint
+
+  element.style.boxSizing = 'border-box';
+  element.style.transitionPropery = 'height, margin, padding';
+  element.style.transitionDuration = `${duration}ms`;
+  element.style.height = `${height}px`;
+  element.style.removeProperty('padding-top');
+  element.style.removeProperty('padding-bottom');
+  element.style.removeProperty('margin-top');
+  element.style.removeProperty('margin-bottom');
+
+  window.setTimeout(() => {
+    element.style.removeProperty('height');
+    element.style.removeProperty('overflow');
+    element.style.removeProperty('transition-duration');
+    element.style.removeProperty('transition-property');
   }, duration);
 }

6ec02a69442610c36774aec6c6bfac8acbabf022
ClaytonDewey <clay@dryadmedia.com>
2025-04-22 14:26:41 -0500

    refactor: Remove if (merchList) check

--- src/merch-list.js
+++ src/merch-list.js
@@ -16,23 +16,22 @@ const noResults = document.createElement('div');
 noResults.classList.add('empty-results');
 
 // Make sure we're on a merchList or newarrivals page
-if (
-  window.location.href.toLowerCase().search(/merchlist/) !== -1 ||
-  window.location.href.toLowerCase().search(/newarrivals/) !== -1
-) {
-  console.log(`We're on the merch list or new arrivals page`);
-  if (categoryTitle.textContent.toLocaleLowerCase() === 'search all') {
-    const searched = window.location.search.split('=');
-    const searchTerm = searched[2].replace(/%20/g, ' ');
-    console.log(`This is a search results page`);
-    // NO RESULTS RETURNED
-    if (noListItems) {
-      console.log(`Search results are empty`);
-      noListItems.style.display = 'none'; // Hide MBS div
-      categoryTitle.style.display = 'none'; // Hide MBS page title
+// if (
+//   window.location.href.toLowerCase().search(/merchlist/) !== -1 ||
+//   window.location.href.toLowerCase().search(/newarrivals/) !== -1
+// ) {
 
-      if (baseUrl === 'https://text.uwbookstore.com/') {
-        noResults.innerHTML = `
+if (categoryTitle.textContent.toLocaleLowerCase() === 'search all') {
+  const searched = window.location.search.split('=');
+  const searchTerm = searched[2].replace(/%20/g, ' ');
+
+  // NO RESULTS RETURNED
+  if (noListItems) {
+    noListItems.style.display = 'none'; // Hide MBS div
+    categoryTitle.style.display = 'none'; // Hide MBS page title
+
+    if (baseUrl === 'https://text.uwbookstore.com/') {
+      noResults.innerHTML = `
         <div class="empty-results">
           <h1>Sorry, we couldn't find any products.</h1>
           <p>We were unable to find results for <strong>${searchTerm}</strong>. Please check your spelling or try searching for similar terms.</p>
@@ -42,31 +41,28 @@ if (
           <a class="btn btn-primary" href="https://text.uwbookstore.com/SelectTermDept">Search Textbooks</a>
         </div>
         `;
-      } else {
-        noResults.innerHTML = `
+    } else {
+      noResults.innerHTML = `
         <div class="empty-results">
           <h1>Sorry, we couldn't find any products.</h1>
           <p>We were unable to find results for <strong>${searchTerm}</strong>. Please check your spelling or try searching for similar terms.</p>
         </div>
         `;
-      }
-      searchCatWrapRow.after(noResults);
     }
+    searchCatWrapRow.after(noResults);
+  }
 
-    categoryTitle.textContent = `Search Results For: ${searchTerm}`;
-  } // END OF if (categoryTitle.toLowerCase() === 'search all')
-  else if (
-    categoryTitle.textContent.toLowerCase() === 'new arrivals' &&
-    noListItems
-  ) {
-    console.log(
-      `We're on the new arrivals page, and there aren't any new items`
-    );
-    noListItems.style.display = 'none'; // Hide MBS div
-    categoryTitle.style.display = 'none'; // Hide MBS page title
+  categoryTitle.textContent = `Search Results For: ${searchTerm}`;
+} // END OF if (categoryTitle.toLowerCase() === 'search all')
+else if (
+  categoryTitle.textContent.toLowerCase() === 'new arrivals' &&
+  noListItems
+) {
+  noListItems.style.display = 'none'; // Hide MBS div
+  categoryTitle.style.display = 'none'; // Hide MBS page title
 
-    if (baseUrl === 'https://www.uwbookstore.com/') {
-      noResults.innerHTML = `
+  if (baseUrl === 'https://www.uwbookstore.com/') {
+    noResults.innerHTML = `
         <section class="empty-results text-center">
           <h1>
             Badger fans, stay tuned&mdash;new gear is coming!
@@ -100,8 +96,8 @@ if (
           </div>
         </section>
       `;
-    } else {
-      noResults.innerHTML = `
+  } else {
+    noResults.innerHTML = `
         <section class="empty-results text-center">
           <h1>
             Badger fans, stay tuned&mdash;new gear is coming!
@@ -111,236 +107,233 @@ if (
           </p>
         </section>
       `;
-    }
-    searchCatWrapRow.after(noResults);
+  }
+  searchCatWrapRow.after(noResults);
+}
+
+if (!noListItems) {
+  // FIX CATEGORY TITLES SINCE MBS HAS CHARACTER LIMITS
+  if (
+    categoryTitle.textContent.toLowerCase().substring(0, 14) ===
+    'dept. of comp.'
+  ) {
+    categoryTitle.textContent = 'Department of Computer Science';
+    document.title = 'Department of Computer Science';
+  } else if (
+    categoryTitle.textContent.toLowerCase().substring(0, 18) ===
+    'college of l and s'
+  ) {
+    categoryTitle.textContent = 'College of Letters and Science';
+    document.title = 'College of Letters and Science';
+  } else if (
+    categoryTitle.textContent.toLowerCase().substring(0, 23) ===
+    'dept of medical physics'
+  ) {
+    categoryTitle.textContent = 'Department of Medical Physics';
+    document.title = 'Department of Medical Physics';
   }
 
-  if (!noListItems) {
-    console.log('.noListItems div is empty');
-    // FIX CATEGORY TITLES SINCE MBS HAS CHARACTER LIMITS
-    if (
-      categoryTitle.textContent.toLowerCase().substring(0, 14) ===
-      'dept. of comp.'
-    ) {
-      categoryTitle.textContent = 'Department of Computer Science';
-      document.title = 'Department of Computer Science';
-    } else if (
-      categoryTitle.textContent.toLowerCase().substring(0, 18) ===
-      'college of l and s'
-    ) {
-      categoryTitle.textContent = 'College of Letters and Science';
-      document.title = 'College of Letters and Science';
-    } else if (
-      categoryTitle.textContent.toLowerCase().substring(0, 23) ===
-      'dept of medical physics'
-    ) {
-      categoryTitle.textContent = 'Department of Medical Physics';
-      document.title = 'Department of Medical Physics';
-    }
+  // HIDE SEARCHCATWRAP PARENT
+  searchCatWrapRow.style.display = 'none';
 
-    // HIDE SEARCHCATWRAP PARENT
-    searchCatWrapRow.style.display = 'none';
+  // CREATE HEADER WRAPPER DIV, ADD CLASS & ID
+  const merchFilterWrap = document.createElement('div');
+  merchFilterWrap.classList.add('merch__filter');
+  merchFilterWrap.id = 'merch__filter';
 
-    // CREATE HEADER WRAPPER DIV, ADD CLASS & ID
-    const merchFilterWrap = document.createElement('div');
-    merchFilterWrap.classList.add('merch__filter');
-    merchFilterWrap.id = 'merch__filter';
+  categoryTitle.style.display = 'none';
 
-    categoryTitle.style.display = 'none';
-
-    merchFilterWrap.innerHTML = `
+  merchFilterWrap.innerHTML = `
       <h2 class="heading__list">${categoryTitle.textContent}</h2>
   `;
 
-    const sortBy = document.createElement('div');
-    sortBy.id = 'sort-by';
-    sortBy.className = 'merch__filter--item';
-    sortBy.innerHTML = `
+  const sortBy = document.createElement('div');
+  sortBy.id = 'sort-by';
+  sortBy.className = 'merch__filter--item';
+  sortBy.innerHTML = `
     <label style="display: block;">Sort By</label>  
   `;
 
-    merchSortBy ? sortBy.appendChild(merchSortBy) : null;
-    merchSortBy ? merchFilterWrap.appendChild(sortBy) : null;
+  merchSortBy ? sortBy.appendChild(merchSortBy) : null;
+  merchSortBy ? merchFilterWrap.appendChild(sortBy) : null;
 
-    const shoppingCartBtn = document.createElement('div');
-    shoppingCartBtn.className = 'merch__filter--item';
-    shoppingCartBtn.innerHTML = `
+  const shoppingCartBtn = document.createElement('div');
+  shoppingCartBtn.className = 'merch__filter--item';
+  shoppingCartBtn.innerHTML = `
     <a class="btn btn-primary" href="https://www.uwbookstore.com/ShoppingCart">
       Shopping Cart
     </a>  
   `;
-    merchFilterWrap.appendChild(shoppingCartBtn);
+  merchFilterWrap.appendChild(shoppingCartBtn);
 
-    // SELECT ELEMENT TO ADD HEADER WRAPPER AFTER
-    filterColumn.after(merchFilterWrap);
+  // SELECT ELEMENT TO ADD HEADER WRAPPER AFTER
+  filterColumn.after(merchFilterWrap);
 
-    // REPLACE MBS's NO IMAGE AVAILABLE GIF
-    itemImage.forEach((image) => {
-      if (image.getAttribute('src') === '/images/notavail.gif') {
-        image.setAttribute(
-          'src',
-          'https://i.univbkstr.com/v3/img/misc/no-image-sm.jpg'
-        );
-        image.setAttribute('alt', 'Image not available');
-      }
-    });
+  // REPLACE MBS's NO IMAGE AVAILABLE GIF
+  itemImage.forEach((image) => {
+    if (image.getAttribute('src') === '/images/notavail.gif') {
+      image.setAttribute(
+        'src',
+        'https://i.univbkstr.com/v3/img/misc/no-image-sm.jpg'
+      );
+      image.setAttribute('alt', 'Image not available');
+    }
+  });
 
-    // RESET MBS MERCHITEM CLASSES AND ADD CUSTOM ONES
-    const merchItems = document.querySelectorAll('.merchItem');
-    merchItems.forEach((item) => {
-      item.className = '';
-      item.classList.add('merchItem', 'merch__card-item');
-    });
+  // RESET MBS MERCHITEM CLASSES AND ADD CUSTOM ONES
+  const merchItems = document.querySelectorAll('.merchItem');
+  merchItems.forEach((item) => {
+    item.className = '';
+    item.classList.add('merchItem', 'merch__card-item');
+  });
 
-    // CREATE THE PRODUCTS WRAPPER - merch__card
-    const merchCard = document.createElement('div');
-    merchCard.id = 'merch__card';
+  // CREATE THE PRODUCTS WRAPPER - merch__card
+  const merchCard = document.createElement('div');
+  merchCard.id = 'merch__card';
 
-    merchFilterWrap.after(merchCard);
+  merchFilterWrap.after(merchCard);
 
-    merchColumn.classList.add('flex', 'merch__card');
-    merchCard.appendChild(merchColumn);
+  merchColumn.classList.add('flex', 'merch__card');
+  merchCard.appendChild(merchColumn);
 
-    merchResultsSelect.classList.remove(
-      'wauto',
-      'displayib',
-      'right5',
-      'bottom10'
-    );
-    merchResultsSelect.classList.add('mx-auto');
+  merchResultsSelect.classList.remove(
+    'wauto',
+    'displayib',
+    'right5',
+    'bottom10'
+  );
+  merchResultsSelect.classList.add('mx-auto');
 
-    const paginationBtm = document.createElement('div');
-    paginationBtm.id = 'pagination-btm';
-    paginationBtm.className = 'text-center';
+  const paginationBtm = document.createElement('div');
+  paginationBtm.id = 'pagination-btm';
+  paginationBtm.className = 'text-center';
 
-    merchCard.after(paginationBtm);
-    paginationBtm.appendChild(pagination);
-    paginationBtm.appendChild(merchResultsSelect);
-    const filterSelectionsRow =
-      document.querySelector('.filterSelections').parentElement;
+  merchCard.after(paginationBtm);
+  paginationBtm.appendChild(pagination);
+  paginationBtm.appendChild(merchResultsSelect);
+  const filterSelectionsRow =
+    document.querySelector('.filterSelections').parentElement;
 
-    filterSelectionsRow ? (filterSelectionsRow.style.display = 'none') : null;
+  filterSelectionsRow ? (filterSelectionsRow.style.display = 'none') : null;
 
-    // ADD BANNERS/MESSAGES TO CERTAIN PAGES
-    // create container for banner/message
-    const pageBanner = document.createElement('div');
+  // ADD BANNERS/MESSAGES TO CERTAIN PAGES
+  // create container for banner/message
+  const pageBanner = document.createElement('div');
 
-    // FOR SCANDINAVIAN STUDIES
-    if (
-      categoryTitle.textContent.toLowerCase().substring(0, 12) ===
-      'scandinavian'
-    ) {
-      pageBanner.classList.add('alert', 'alert-warning');
-      pageBanner.innerHTML = `
+  // FOR SCANDINAVIAN STUDIES
+  if (
+    categoryTitle.textContent.toLowerCase().substring(0, 12) === 'scandinavian'
+  ) {
+    pageBanner.classList.add('alert', 'alert-warning');
+    pageBanner.innerHTML = `
       <p style="text-align: center; margin: 0; font-size: 20px;">
         <em>Note</em> - All books sold in packs of five &mdash; price listed is for five books.
       </p>    
     `;
-      merchFilterWrap.after(pageBanner);
-    }
+    merchFilterWrap.after(pageBanner);
+  }
 
-    // FOR NEW ARRIVALS
-    if (categoryTitle.textContent.toLowerCase() === 'new arrivals') {
-      pageBanner.classList.add('text-center');
-      pageBanner.innerHTML = `
+  // FOR NEW ARRIVALS
+  if (categoryTitle.textContent.toLowerCase() === 'new arrivals') {
+    pageBanner.classList.add('text-center');
+    pageBanner.innerHTML = `
     <a href="https://www.uwbookstore.com/Wisconsin-Badgers/gift-items/New-Items" class="btn btn-primary">
     Back in Stock Items
     </a>
     `;
-      merchFilterWrap.after(pageBanner);
-    }
+    merchFilterWrap.after(pageBanner);
+  }
 
-    // FOR AM FAM CHAMPIONSHIP ITEMS
-    if (categoryTitle.textContent.toLowerCase().substring(0, 6) === 'am fam') {
-      pageBanner.innerHTML = `
+  // FOR AM FAM CHAMPIONSHIP ITEMS
+  if (categoryTitle.textContent.toLowerCase().substring(0, 6) === 'am fam') {
+    pageBanner.innerHTML = `
       <img src="https://i.univbkstr.com/v3/img/landing/merch/AmFamGolf.png" class="img-fluid img__center d-block" alt="AmFam Championship. Official Merchandise Partner of the American Family Insurance Championship.">   
     `;
-      filterColumn.after(pageBanner);
-    }
+    filterColumn.after(pageBanner);
+  }
 
-    // FOR JULIA GASH ITEMS
-    if (
-      categoryTitle.textContent.toLowerCase().substring(0, 10) === 'julia gash'
-    ) {
-      pageBanner.innerHTML = `
+  // FOR JULIA GASH ITEMS
+  if (
+    categoryTitle.textContent.toLowerCase().substring(0, 10) === 'julia gash'
+  ) {
+    pageBanner.innerHTML = `
       <img alt="Julia Gash" src="https://i.univbkstr.com/v3/img/banners/JuliaGash.png" class="img-fluid img__center d-block">
     `;
-      filterColumn.after(pageBanner);
-    }
+    filterColumn.after(pageBanner);
+  }
 
-    // SPECIAL PRICING NOTE FOR ELECTRONICS
-    if (categoryTitle.textContent.toLowerCase().substring(0, 4) === 'ipad') {
-      pageBanner.classList.add('alert', 'alert-info', 'text-center');
-      pageBanner.innerHTML = `
+  // SPECIAL PRICING NOTE FOR ELECTRONICS
+  if (categoryTitle.textContent.toLowerCase().substring(0, 4) === 'ipad') {
+    pageBanner.classList.add('alert', 'alert-info', 'text-center');
+    pageBanner.innerHTML = `
         <p class="mb-0"><strong>The price displayed is our special educational price available to UW Students, Faculty, Staff, Alumni, &amp;&nbsp;UW&nbsp;Health Employees.</strong></p>
       `;
-      merchFilterWrap.after(pageBanner);
-    }
+    merchFilterWrap.after(pageBanner);
+  }
 
-    // ADD MACBOOK/DELL CROSSLINKS
-    if (baseUrl === 'https://www.uwbookstore.com/') {
-      if (
-        categoryTitle.textContent.toLowerCase().substring(0, 16) ===
-        'dell laptop'
-      ) {
-        pageBanner.innerHTML = `
+  // ADD MACBOOK/DELL CROSSLINKS
+  if (baseUrl === 'https://www.uwbookstore.com/') {
+    if (
+      categoryTitle.textContent.toLowerCase().substring(0, 16) === 'dell laptop'
+    ) {
+      pageBanner.innerHTML = `
         <ul class="page-nav"><li><a href="https://www.uwbookstore.com/Wisconsin-Badgers/Tech/Laptops" class="btn btn-primary">All Laptops</a></li><li><a href="https://www.uwbookstore.com/Wisconsin-Badgers/Tech/MacBook" class="btn btn-primary">MacBooks</a></li></ul>
         <div class="alert alert-info text-center">
           <p class="mb-0"><strong>The price displayed is our special educational price available to UW Students, Faculty, Staff, Alumni, &amp;&nbsp;UW&nbsp;Health Employees.</strong></p>
         </div>
       `;
-        merchFilterWrap.after(pageBanner);
-      } else if (
-        categoryTitle.textContent.toLowerCase().substring(0, 7) === 'macbook'
-      ) {
-        pageBanner.innerHTML = `
+      merchFilterWrap.after(pageBanner);
+    } else if (
+      categoryTitle.textContent.toLowerCase().substring(0, 7) === 'macbook'
+    ) {
+      pageBanner.innerHTML = `
         <ul class="page-nav"><li><a href="https://www.uwbookstore.com/Wisconsin-Badgers/Tech/Laptops" class="btn btn-primary">All Laptops</a></li><li><a href="https://www.uwbookstore.com/Wisconsin-Badgers/Tech/Dell-Laptop" class="btn btn-primary">Dell Laptops</a></li></ul>
         <div class="alert alert-info text-center">
           <p class="mb-0"><strong>The price displayed is our special educational price available to UW Students, Faculty, Staff, Alumni, &amp;&nbsp;UW&nbsp;Health Employees.</strong></p>
         </div>
       `;
-        merchFilterWrap.after(pageBanner);
-      } else if (
-        categoryTitle.textContent.toLowerCase().substring(0, 6) === 'laptop'
-      ) {
-        pageBanner.innerHTML = `
+      merchFilterWrap.after(pageBanner);
+    } else if (
+      categoryTitle.textContent.toLowerCase().substring(0, 6) === 'laptop'
+    ) {
+      pageBanner.innerHTML = `
         <ul class="page-nav"><li><a href="https://www.uwbookstore.com/Wisconsin-Badgers/Tech/MacBook" class="btn btn-primary">MacBooks</a></li><li><a href="https://www.uwbookstore.com/Wisconsin-Badgers/Tech/Dell-Laptop" class="btn btn-primary">Dell Laptops</a></li></ul>
         <div class="alert alert-info text-center">
           <p class="mb-0"><strong>The price displayed is our special educational price available to UW Students, Faculty, Staff, Alumni, &amp;&nbsp;UW&nbsp;Health Employees.</strong></p>
         </div>
       `;
-        merchFilterWrap.after(pageBanner);
-      }
+      merchFilterWrap.after(pageBanner);
     }
+  }
 
-    // ADD RED SHIRT HELP BUTTON
-    if (
-      window.location.href.toLowerCase().indexOf('red%20shirt') > -1 ||
-      window.location.href.indexOf('17th') > -1
-    ) {
-      pageBanner.classList.add('p-0', 'text-center');
-      pageBanner.innerHTML = `
+  // ADD RED SHIRT HELP BUTTON
+  if (
+    window.location.href.toLowerCase().indexOf('red%20shirt') > -1 ||
+    window.location.href.indexOf('17th') > -1
+  ) {
+    pageBanner.classList.add('p-0', 'text-center');
+    pageBanner.innerHTML = `
       Looking for The Red Shirt&trade;, 17<sup>th</sup> Edition? Find it here:<br>
       <a href="https://www.uwbookstore.com/Wisconsin-Badgers/gift-items/The-Red-Shirt-17th-Edition" class="btn btn-primary m-2">The Red Shirt&trade;</a>    
     `;
-      filterColumn.after(pageBanner);
-    }
-
-    // ADD BACK IN STOCK BADGE TO ITEMS WITH ^ IN PRODUCTNAME
-    productName.forEach((item) => {
-      const name = item.textContent;
-
-      if (name.includes('^')) {
-        const restockBadge = document.createElement('div');
-        restockBadge.className = 'restockBadge';
-        restockBadge.innerHTML = 'Back in<br>Stock!';
-
-        const merchLink = item.closest('.merchDetailWrapper');
-        merchLink.prepend(restockBadge);
-      }
-    });
+    filterColumn.after(pageBanner);
   }
-} // END OF MERCHLIST IF STATEMENT
+
+  // ADD BACK IN STOCK BADGE TO ITEMS WITH ^ IN PRODUCTNAME
+  productName.forEach((item) => {
+    const name = item.textContent;
+
+    if (name.includes('^')) {
+      const restockBadge = document.createElement('div');
+      restockBadge.className = 'restockBadge';
+      restockBadge.innerHTML = 'Back in<br>Stock!';
+
+      const merchLink = item.closest('.merchDetailWrapper');
+      merchLink.prepend(restockBadge);
+    }
+  });
+}
+// } END OF MERCHLIST IF STATEMENT
 
 // USE JQUERY TO CHECK PAGE ITEMS LENGTH
 // IF ONLY ONE PAGE OF RESULTS, HIDE PAGINATION

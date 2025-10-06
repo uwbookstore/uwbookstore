aeb32da895563c26479eacdb6122831ab2250bdb
Clayton Dewey <clay@dryadmedia.com>
2025-10-06 07:53:47 -0500

    chore: Update rotators & countdown

--- src/countdown.js
+++ src/countdown.js
@@ -26,7 +26,7 @@ const countdownWrapper = document.querySelector('.countdown__wrapper');
 const items = document.querySelectorAll('.timer__box p');
 
 // months are ZERO index based;
-const futureDate = new Date(Date.UTC(2025, 9, 11, 16, 0, 0));
+const futureDate = new Date(Date.UTC(2025, 9, 11, 23, 0, 0));
 // console.log(futureDate);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();

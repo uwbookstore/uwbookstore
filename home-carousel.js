f3afc095f140339cb5c4eb381144bf14528689a0
ClaytonDewey <clay@dryadmedia.com>
2025-10-13 16:10:41 -0500

    test: Rework carousel

--- home-carousel.js
+++ home-carousel.js
@@ -1,39 +1,23 @@
 window.CAROUSEL_CONFIG = {
-  title: 'Homepage Carousel',
-  options: { autoplay_ms: 5000, loop: true },
+  options: { autoplay_ms: 5000, loop: true, fade: true },
   slides: [
     {
-      src: 'https://i.univbkstr.com/v3/img/pages/home/rotator/buckyBday25-lg.jpg',
-      alt: 'Welcome',
-      caption: '',
-      sr_caption:
-        'Limited-time welcome promotion for new students. See details on the linked page.',
+      imgName: 'badgerHeadquarters',
+      alt: '',
+      sr_caption: 'Your Badger Headquarters! Shop New Arrivals!',
       link: 'https://www.uwbookstore.com/NewArrivals',
       target: '_self',
       start: '2025-10-08T00:00:00-05:00',
-      end: '2025-10-01T23:59:59-05:00',
+      end: '2025-10-13T23:59:59-05:00',
     },
     {
-      src: 'https://i.univbkstr.com/v3/img/pages/home/rotator/flashSale-lg.jpg',
-      alt: 'Specials',
-      caption: 'Fall textbook buyback is on!',
-      sr_caption:
-        'Limited-time welcome promotion for new students. See details on the linked page.',
-      link: 'https://www.uwbookstore.com/NewArrivals',
-      target: '_self',
-      start: '2025-10-01T10:27:00-05:00',
-      end: '2025-10-10T10:27:00-05:00',
-    },
-    {
-      src: 'https://i.univbkstr.com/v3/img/pages/home/rotator/packJacket-lg.jpg',
-      alt: 'Campus life',
-      caption: 'Gear up for game day',
-      sr_caption:
-        'Limited-time welcome promotion for new students. See details on the linked page.',
-      link: 'https://www.uwbookstore.com/NewArrivals',
+      imgName: 'hockeyRotator',
+      alt: '',
+      sr_caption: 'Wisconsin Hockey is Back! Get your gear here!',
+      link: 'https://www.uwbookstore.com/Wisconsin-Badgers/gift-items/Hockey',
       target: '_self',
       start: '2025-10-08T00:00:00-05:00',
-      end: '2025-10-10T23:59:59-05:00',
+      end: '2025-12-10T23:59:59-05:00',
     },
   ],
 };

fbf1626821fe1f495c5cbaa4bdd5e70661458d70
ClaytonDewey <clay@dryadmedia.com>
2025-12-29 14:03:58 -0600

    refactor: Rewrite as vanilla JS

--- src/smooth-scroll.js
+++ src/smooth-scroll.js
@@ -1,15 +1,19 @@
-$(function() {
-  $("a.page-scroll").bind("click", function() {
-    var $anchor = $(this);
-    $("html, body")
-      .stop()
-      .animate(
-        {
-          scrollTop: $($anchor.attr("href")).offset().top
-        },
-        1500,
-        "easeInOutExpo"
-      );
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+  const links = document.querySelectorAll("a.page-scroll[href^='#']");
+
+  links.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      }
+    });
   });
 });

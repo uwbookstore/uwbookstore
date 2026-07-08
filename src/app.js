5d3cb8ce8acc7d98a04736506fe89c486e41659b
ClaytonDewey <clay@dryadmedia.com>
2026-07-08 11:14:29 -0500

    fix: Add play/pause controls for footer marquee

--- src/app.js
+++ src/app.js
@@ -420,6 +420,39 @@ footerCopyright
 `)
   : null;
 
+// PAUSE/PLAY CONTROLS FOR FOOTER MARQUEE
+const marqueeControl = document.getElementById('marquee-play-pause');
+const marqueeTrack = document.querySelector('.marquee-content-track');
+
+function toggleMarquee() {
+  if (marqueeControl.getAttribute('aria-pressed') === 'false') {
+    marqueeTrack.style.animationPlayState = 'paused';
+    marqueeControl.setAttribute('aria-pressed', 'true');
+    marqueeControl.innerHTML = `
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+            <path fill-rule="evenodd"
+              d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+              clip-rule="evenodd" />
+          </svg>
+        `;
+  } else {
+    marqueeTrack.style.animationPlayState = 'running';
+    marqueeControl.setAttribute('aria-pressed', 'false');
+    marqueeControl.innerHTML = `
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6"
+            aria-hidden="true">
+            <path fill-rule="evenodd"
+              d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z"
+              clip-rule="evenodd" />
+          </svg>
+        `;
+  }
+}
+
+marqueeControl.addEventListener('click', () => {
+  toggleMarquee();
+});
+
 // TODO: Look at Order Details page
 // Create the order info div and insert after h1#ordersHeader
 const orderInfo = document.createElement('div');

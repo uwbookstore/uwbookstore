bba63f12711d1622950da555a0b39645532a7c97
ClaytonDewey <clay@dryadmedia.com>
2026-09-15 15:21:02 -0500

    chore: Update Lands' End disclaimer

--- src/new-merch-detail.js
+++ src/new-merch-detail.js
@@ -438,7 +438,7 @@ if (merchDisclaimerFormCheck) {
   // LANDS' END DISCLAIMER
   else if (document.getElementById('landsEndReally')) {
     disclaimerText.innerHTML = `
-      <strong>PLEASE READ BEFORE PURCHASE! — Lands&apos; End may take 10-15 business days (M-F) to ship. Lands&rsquo; End orders will incur a $10 handling fee due to it being drop shipped from the manufacturer.</strong> This custom item is <strong>NOT ELIGIBLE</strong> for <strong>returns or exchanges</strong> and does not qualify for <strong>expedited or free shipping. By clicking this box, you are agreeing to these terms.</strong>
+      <strong>PLEASE READ BEFORE PURCHASE! — Lands&apos; End may take 17-22 business days (M-F) to ship. Lands&rsquo; End orders will incur a $10 handling fee due to it being drop shipped from the manufacturer.</strong> This custom item is <strong>NOT ELIGIBLE</strong> for <strong>returns or exchanges</strong> and does not qualify for <strong>expedited or free shipping. By clicking this box, you are agreeing to these terms.</strong>
     `;
   } // KYLE CAVAN DISCLAIMER
   else if (document.getElementById('kyleCavan')) {
